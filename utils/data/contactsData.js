a0182c46d452c8172ae4376a186220e0c1eccba8
mfaris2002 <79490962+mfaris2002@users.noreply.github.com>
2025-07-04 15:33:03 +0700

    Huge update

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'farisrafi.jobs@gmail.com',
+    phone: '+6282319523935',
+    address: 'Bandung, West Java, Indonesia',
+    github: 'https://github.com/said7388',
+    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "said7388"
+}
