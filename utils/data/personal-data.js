ef16cefdf7a7aa46eb3e10888cd30af6cfc8edbc
mfaris2002 <79490962+mfaris2002@users.noreply.github.com>
2025-07-04 16:10:47 +0700

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,17 @@
 export const personalData = {
-  name: "ABU SAID",
+  name: "Muhamad Faris Rafi",
   profile: '/profile.png',
-  designation: "Software Developer",
+  designation: "Fullstack Developer",
   description: "My name is Muhamad Faris Rafi. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
-  phone: '+8801608797655',
-  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-  github: 'https://github.com/said7388',
+  email: 'farisrafi.jobs@gmail.com',
+  phone: '+6282319523935',
+  address: 'Bandung, Jawa Barat',
+  github: 'https://github.com/mfaris2002',
   facebook: 'https://www.facebook.com/abusaid.riyaz/',
   linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
   twitter: 'https://twitter.com/said7388',
   stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
   leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  devUsername: "mfaris2002",
+  resume: "https://drive.google.com/"
 }
